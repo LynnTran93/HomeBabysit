27b939319ee5b506dd6fe9ed6a0db15052e76b62
LynnTran93 <tran0502@algonquinlive.com>
2024-09-30 17:24:35 -0400

    Initial commit for HomeBabysit project

---
+++ app/src/main/java/com/example/homebabysit/DatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.example.homebabysit;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    // Database Name and Version
+    private static final String DATABASE_NAME = "homebabysit.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table Names
+    public static final String TABLE_USERS = "users";
+    public static final String TABLE_BABYSITTERS = "babysitters";
+    public static final String TABLE_BOOKINGS = "bookings";
+
+    // Users Table Columns
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_USER_NAME = "name";
+    public static final String COLUMN_USER_EMAIL = "email";
+    public static final String COLUMN_USER_PASSWORD = "password";
+
+    // Babysitters Table Columns
+    public static final String COLUMN_BABYSITTER_ID = "babysitter_id";
+    public static final String COLUMN_BABYSITTER_NAME = "name";
+    public static final String COLUMN_BABYSITTER_EXPERIENCE = "experience";
+    public static final String COLUMN_BABYSITTER_RATING = "rating";
+
+    // SQL to Create Users Table
+    private static final String CREATE_TABLE_USERS = "CREATE TABLE " + TABLE_USERS + "("
+            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_USER_NAME + " TEXT, "
+            + COLUMN_USER_EMAIL + " TEXT, "
+            + COLUMN_USER_PASSWORD + " TEXT" + ")";
+
+    // SQL to Create Babysitters Table
+    private static final String CREATE_TABLE_BABYSITTERS = "CREATE TABLE " + TABLE_BABYSITTERS + "("
+            + COLUMN_BABYSITTER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_BABYSITTER_NAME + " TEXT, "
+            + COLUMN_BABYSITTER_EXPERIENCE + " INTEGER, "
+            + COLUMN_BABYSITTER_RATING + " REAL" + ")";
+
+    // SQL to Create Bookings Table
+    private static final String CREATE_TABLE_BOOKINGS = "CREATE TABLE " + TABLE_BOOKINGS + "("
+            + "booking_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + "parent_id INTEGER, "
+            + "babysitter_id INTEGER, "
+            + "date TEXT, "
+            + "FOREIGN KEY (parent_id) REFERENCES " + TABLE_USERS + "(" + COLUMN_USER_ID + "), "
+            + "FOREIGN KEY (babysitter_id) REFERENCES " + TABLE_BABYSITTERS + "(" + COLUMN_BABYSITTER_ID + "))";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Creating the tables when the database is created
+        db.execSQL(CREATE_TABLE_USERS);
+        db.execSQL(CREATE_TABLE_BABYSITTERS);
+        db.execSQL(CREATE_TABLE_BOOKINGS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older tables if they exist
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BABYSITTERS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BOOKINGS);
+
+        // Create new tables
+        onCreate(db);
+    }
+}
