f993350c9143f7fbd6474c2c0e74dd7113ea9c60
LynnTran93 <tran0502@algonquinlive.com>
2024-10-01 10:11:24 -0400

    Database

--- app/src/main/java/com/example/homebabysit/DatabaseHelper.java
+++ app/src/main/java/com/example/homebabysit/DatabaseHelper.java
@@ -19,35 +19,59 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_USER_NAME = "name";
     public static final String COLUMN_USER_EMAIL = "email";
     public static final String COLUMN_USER_PASSWORD = "password";
+    public static final String COLUMN_USER_LOCATION = "location";  // New
+    public static final String COLUMN_USER_CHILDREN = "num_children";  // New
+    public static final String COLUMN_USER_PREFERENCES = "preferences";  // New
 
     // Babysitters Table Columns
     public static final String COLUMN_BABYSITTER_ID = "babysitter_id";
     public static final String COLUMN_BABYSITTER_NAME = "name";
+    public static final String COLUMN_BABYSITTER_QUALIFICATIONS = "qualifications";  // New
     public static final String COLUMN_BABYSITTER_EXPERIENCE = "experience";
     public static final String COLUMN_BABYSITTER_RATING = "rating";
+    public static final String COLUMN_BABYSITTER_AVAILABILITY = "availability";  // New
+    public static final String COLUMN_BABYSITTER_RATE = "hourly_rate";  // New
+    public static final String COLUMN_BABYSITTER_PHOTO = "profile_photo";  // New
+
+    // Bookings Table Columns
+    public static final String COLUMN_BOOKING_ID = "booking_id";
+    public static final String COLUMN_PARENT_ID = "parent_id";
+    public static final String COLUMN_BABYSITTER_ID = "babysitter_id";
+    public static final String COLUMN_BOOKING_DATE = "date";
+    public static final String COLUMN_BOOKING_TIME_SLOT = "time_slot";  // New
+    public static final String COLUMN_BOOKING_STATUS = "status";  // New
 
     // SQL to Create Users Table
     private static final String CREATE_TABLE_USERS = "CREATE TABLE " + TABLE_USERS + "("
             + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + COLUMN_USER_NAME + " TEXT, "
             + COLUMN_USER_EMAIL + " TEXT, "
-            + COLUMN_USER_PASSWORD + " TEXT" + ")";
+            + COLUMN_USER_PASSWORD + " TEXT, "
+            + COLUMN_USER_LOCATION + " TEXT, "
+            + COLUMN_USER_CHILDREN + " INTEGER, "
+            + COLUMN_USER_PREFERENCES + " TEXT" + ")";
 
     // SQL to Create Babysitters Table
     private static final String CREATE_TABLE_BABYSITTERS = "CREATE TABLE " + TABLE_BABYSITTERS + "("
             + COLUMN_BABYSITTER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + COLUMN_BABYSITTER_NAME + " TEXT, "
+            + COLUMN_BABYSITTER_QUALIFICATIONS + " TEXT, "
             + COLUMN_BABYSITTER_EXPERIENCE + " INTEGER, "
-            + COLUMN_BABYSITTER_RATING + " REAL" + ")";
+            + COLUMN_BABYSITTER_RATING + " REAL, "
+            + COLUMN_BABYSITTER_AVAILABILITY + " TEXT, "
+            + COLUMN_BABYSITTER_RATE + " REAL, "
+            + COLUMN_BABYSITTER_PHOTO + " BLOB" + ")";  // Assuming profile photo is stored as a blob
 
     // SQL to Create Bookings Table
     private static final String CREATE_TABLE_BOOKINGS = "CREATE TABLE " + TABLE_BOOKINGS + "("
-            + "booking_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + "parent_id INTEGER, "
-            + "babysitter_id INTEGER, "
-            + "date TEXT, "
-            + "FOREIGN KEY (parent_id) REFERENCES " + TABLE_USERS + "(" + COLUMN_USER_ID + "), "
-            + "FOREIGN KEY (babysitter_id) REFERENCES " + TABLE_BABYSITTERS + "(" + COLUMN_BABYSITTER_ID + "))";
+            + COLUMN_BOOKING_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_PARENT_ID + " INTEGER, "
+            + COLUMN_BABYSITTER_ID + " INTEGER, "
+            + COLUMN_BOOKING_DATE + " TEXT, "
+            + COLUMN_BOOKING_TIME_SLOT + " TEXT, "
+            + COLUMN_BOOKING_STATUS + " TEXT, "
+            + "FOREIGN KEY (" + COLUMN_PARENT_ID + ") REFERENCES " + TABLE_USERS + "(" + COLUMN_USER_ID + "), "
+            + "FOREIGN KEY (" + COLUMN_BABYSITTER_ID + ") REFERENCES " + TABLE_BABYSITTERS + "(" + COLUMN_BABYSITTER_ID + "))";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
