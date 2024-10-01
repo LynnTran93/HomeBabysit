066ad9230b428dd76d817afc2df06c9e97509d4c
LynnTran93 <tran0502@algonquinlive.com>
2024-10-01 10:38:38 -0400

    database

--- app/src/main/java/com/example/homebabysit/DatabaseHelper.java
+++ app/src/main/java/com/example/homebabysit/DatabaseHelper.java
@@ -19,17 +19,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_USER_NAME = "name";
     public static final String COLUMN_USER_EMAIL = "email";
     public static final String COLUMN_USER_PASSWORD = "password";
-<<<<<<< HEAD
     public static final String COLUMN_USER_LOCATION = "location";  // New
     public static final String COLUMN_USER_CHILDREN = "num_children";  // New
     public static final String COLUMN_USER_PREFERENCES = "preferences";  // New
-=======
->>>>>>> 6bb0bc72d039b8bfa2dc77ef3f9272249b6bb3a7
 
     // Babysitters Table Columns
     public static final String COLUMN_BABYSITTER_ID = "babysitter_id";
     public static final String COLUMN_BABYSITTER_NAME = "name";
-<<<<<<< HEAD
     public static final String COLUMN_BABYSITTER_QUALIFICATIONS = "qualifications";  // New
     public static final String COLUMN_BABYSITTER_EXPERIENCE = "experience";
     public static final String COLUMN_BABYSITTER_RATING = "rating";
@@ -42,30 +38,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_BOOKING_DATE = "date";
     public static final String COLUMN_BOOKING_TIME_SLOT = "time_slot";  // New
     public static final String COLUMN_BOOKING_STATUS = "status";  // New
-=======
-    public static final String COLUMN_BABYSITTER_EXPERIENCE = "experience";
-    public static final String COLUMN_BABYSITTER_RATING = "rating";
->>>>>>> 6bb0bc72d039b8bfa2dc77ef3f9272249b6bb3a7
 
     // SQL to Create Users Table
     private static final String CREATE_TABLE_USERS = "CREATE TABLE " + TABLE_USERS + "("
             + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + COLUMN_USER_NAME + " TEXT, "
             + COLUMN_USER_EMAIL + " TEXT, "
-<<<<<<< HEAD
             + COLUMN_USER_PASSWORD + " TEXT, "
             + COLUMN_USER_LOCATION + " TEXT, "
             + COLUMN_USER_CHILDREN + " INTEGER, "
             + COLUMN_USER_PREFERENCES + " TEXT" + ")";
-=======
-            + COLUMN_USER_PASSWORD + " TEXT" + ")";
->>>>>>> 6bb0bc72d039b8bfa2dc77ef3f9272249b6bb3a7
 
     // SQL to Create Babysitters Table
     private static final String CREATE_TABLE_BABYSITTERS = "CREATE TABLE " + TABLE_BABYSITTERS + "("
             + COLUMN_BABYSITTER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + COLUMN_BABYSITTER_NAME + " TEXT, "
-<<<<<<< HEAD
             + COLUMN_BABYSITTER_QUALIFICATIONS + " TEXT, "
             + COLUMN_BABYSITTER_EXPERIENCE + " INTEGER, "
             + COLUMN_BABYSITTER_RATING + " REAL, "
@@ -83,19 +70,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             + COLUMN_BOOKING_STATUS + " TEXT, "
             + "FOREIGN KEY (" + COLUMN_USER_ID + ") REFERENCES " + TABLE_USERS + "(" + COLUMN_USER_ID + "), "
             + "FOREIGN KEY (" + COLUMN_BABYSITTER_ID + ") REFERENCES " + TABLE_BABYSITTERS + "(" + COLUMN_BABYSITTER_ID + "))";
-=======
-            + COLUMN_BABYSITTER_EXPERIENCE + " INTEGER, "
-            + COLUMN_BABYSITTER_RATING + " REAL" + ")";
-
-    // SQL to Create Bookings Table
-    private static final String CREATE_TABLE_BOOKINGS = "CREATE TABLE " + TABLE_BOOKINGS + "("
-            + "booking_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + "parent_id INTEGER, "
-            + "babysitter_id INTEGER, "
-            + "date TEXT, "
-            + "FOREIGN KEY (parent_id) REFERENCES " + TABLE_USERS + "(" + COLUMN_USER_ID + "), "
-            + "FOREIGN KEY (babysitter_id) REFERENCES " + TABLE_BABYSITTERS + "(" + COLUMN_BABYSITTER_ID + "))";
->>>>>>> 6bb0bc72d039b8bfa2dc77ef3f9272249b6bb3a7
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -119,8 +93,4 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         // Create new tables
         onCreate(db);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6bb0bc72d039b8bfa2dc77ef3f9272249b6bb3a7
